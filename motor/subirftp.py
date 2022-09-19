48b71e7ff3b2dc0e335f1ab2f2db70bce6d393be
Your Name <you@example.com>
2022-09-19 13:10:35 +0200

    configuracion inicial

--- motor/subirftp.py
+++ motor/subirftp.py
@@ -1,12 +1,20 @@
 import pysftp
 cnopts = pysftp.CnOpts()
-cnopts.hostkeys = None   
-myHostname = "217.61.112.100"
+cnopts.hostkeys = None 
+
+
+
+# myHostname = "217.61.112.100"
+# myUsername = "testuser"
+# myPassword = "prueba123"
+
+myHostname = "camarasdef.vps.webdock.io"
 myUsername = "testuser"
 myPassword = "prueba123"
 
+
 with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword, cnopts=cnopts) as sftp:
     print ("Connection succesfully stablished ... ")
-    sftp.put('output.out','tets.out')
+    sftp.put("/var/www/html/reconocimientofacialV2/motor/videos/1/1_2022-09-19_11:03:54.107124.avi",'motor/videos/1/1/video.avi')
     sftp.close()
-os.remove(video_actual)
+# os.remove(video_actual)
