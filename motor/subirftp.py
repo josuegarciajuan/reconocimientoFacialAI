6b29fca2cd884d45bf5c712d86ca686e3dee99a6
Your Name <you@example.com>
2022-09-19 13:40:12 +0200

    configuracion inicial

--- motor/subirftp.py
+++ motor/subirftp.py
@@ -17,7 +17,7 @@ myUsername = "testuser"
 myPassword = "prueba123"
 
 
-with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword, allow_agent=False, look_for_keys=False , cnopts=cnopts) as sftp:
+with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword, cnopts=cnopts) as sftp:
     print ("Connection succesfully stablished ... ")
     sftp.put("/var/www/html/reconocimientofacialV2/motor/videos/1/1_2022-09-19_11:03:54.107124.avi",'motor/videos/1/1/video.avi')
     sftp.close()
