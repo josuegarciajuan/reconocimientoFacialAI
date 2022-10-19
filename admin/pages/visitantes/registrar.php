7b453017cf73c6f7e275f04180dd2a60f83e8b1b
Your Name <you@example.com>
2022-10-19 17:40:15 +0200

    pruebas

--- admin/pages/visitantes/registrar.php
+++ admin/pages/visitantes/registrar.php
@@ -365,7 +365,7 @@
             
             var fd = new FormData();
             //fd.append("imagen",blob,"imagen.png");
-            fd.append("imagen",blob);
+            //fd.append("imagen",blob);
             fd.append("campo","valor");
             //fd.append("video2",recordedMediaURL);
             var request = new XMLHttpRequest();
