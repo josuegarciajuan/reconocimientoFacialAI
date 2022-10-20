36cd159cc8f5d5385565390486cad602677fa9d3
Your Name <you@example.com>
2022-10-20 17:37:12 +0200

    pruebas

--- admin/pages/visitantes/registrar.php
+++ admin/pages/visitantes/registrar.php
@@ -419,10 +419,7 @@
             
             //request.open("POST", "<?= URL_PROGRAMA_SERVER ?>admin/index.php?page=visitantes&mode=registrar&info=checkearPosicionCara&posicion=" + posicion+"&debug=1", false);
 
-
-
-
-            request.open("POST", "index.php?page=visitantes&mode=registrar&info=checkearPosicionCara2&posicion=" + posicion+"&debug=1", true);
+            request.open("POST", "index.php?page=visitantes&mode=registrar&info=checkearPosicionCara2&posicion=" + posicion+"&debug=1", false);
             //request.setRequestHeader('Content-Type', 'multipart/form-data;');
             //request.send(fd);
             request.send(blob);
