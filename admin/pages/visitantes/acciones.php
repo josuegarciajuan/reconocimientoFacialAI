8af85e015b7cc776707cfe257a7a92fd447ce967
Your Name <you@example.com>
2022-10-20 17:45:13 +0200

    pruebas

--- admin/pages/visitantes/acciones.php
+++ admin/pages/visitantes/acciones.php
@@ -278,7 +278,7 @@ if(isset($_GET["info"]) and $_GET["info"]=="checkearPosicionCara2"){
     $terminado=false;
     $fichero_respuesta=$res_dir."/".$_SESSION["local_id"].".txt";
     
-    $espera=15;
+    $espera=30;
     $e=0;
     while(!$terminado){
         if(file_exists($fichero_respuesta)){
@@ -290,8 +290,8 @@ if(isset($_GET["info"]) and $_GET["info"]=="checkearPosicionCara2"){
             sleep(1);
             $e++;
             if($e>$espera){
-                //$terminado=true;
-                //$respuesta="---resp>NOOK<---";
+                $terminado=true;
+                $respuesta="---resp>NOOK<---";
             }
         }
     }
