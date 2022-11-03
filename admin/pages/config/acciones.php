bbaf6349f4a07f0d32042be25c33e354215ce9ad
Your Name <you@example.com>
2022-11-03 10:57:01 +0100

    con lo de sensibiliada de mov

--- admin/pages/config/acciones.php
+++ admin/pages/config/acciones.php
@@ -66,7 +66,7 @@ switch($_GET["accion"]){
         $url_conexion= str_replace("--jos--", "&", $_GET["url_conexion"]);
         $url_desdeserver= str_replace("--jos--", "&", $_GET["url_desdeserver"]);
         
-        $campos=["local_id","descripcion","url_conexion","sistema","puerta","salida","encendida","x","y","ipcamlive_alias","url_desdeserver","sensibilidad","sensibilidad_movimiento"];
+        $campos=["local_id","descripcion","url_conexion","sistema","puerta","salida","encendida","x","y","ipcamlive_alias","url_desdeserver","sensibilidad_movimiento"];
         $valores=[$_SESSION["local_id"],$_GET["nombre"],$url_conexion,$_GET["sistema"],$_GET["puerta"],$_GET["salida"],$_GET["encendida"],$_GET["x"],$_GET["y"],$_GET["ipcamlive_alias"],$url_desdeserver,$_GET["sensibilidad_movimiento"]];
         
         $sql->Actualizar("camaras",$campos,$valores,"id=".$_GET["camara"],false);
