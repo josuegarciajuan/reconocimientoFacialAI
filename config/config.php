c5a8f8067c270a0799bd34442f894e2a03a75fdc
Your Name <you@example.com>
2022-11-07 16:59:58 +0100

    mas ajustes al controlador de movimiento

--- config/config.php
+++ config/config.php
@@ -93,7 +93,8 @@ define("CONFIG_LIMITE_RAM",85); //limite de ram ocupada para seguir procesando v
 define("CONFIG_LIMITE_VIDEOS",25); //numero de videos maximo que se pueden procesar a la vez
 define("CONFIG_TIEMPOPROCESODECLISIFICARCARAS",60*10);  //en segs tiempo max q puede estar el proceso de procesar caras, luego se reinicia
 //procesa_videosV6.py
-define("CONFIG_SENSIBILIDAD_ES_CARA","0.68");
+//define("CONFIG_SENSIBILIDAD_ES_CARA","0.68");
+define("CONFIG_SENSIBILIDAD_ES_CARA","0.5");
 define("CONFIG_desiredFaceWidth","150"); //alinear cara desiredFaceWidth  l.72
 define("CONFIG_margen_cruce_linea","5"); //margen cruce linea l.139
 define("CONFIG_frame_rate","10"); //frame_rate = 10 #cuanto mas alto menos fluido va, osea mas delay, y detecta mas rapido   l.200
