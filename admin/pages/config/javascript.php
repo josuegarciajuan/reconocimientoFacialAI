a35df099eb8ef925ec502ac6a143cafcf8d7b1f2
Josue <josue@nouesmalt.com>
2024-02-13 12:51:34 +0100

    ajustes para dibujar las lineas de las fotos

--- admin/pages/config/javascript.php
+++ admin/pages/config/javascript.php
@@ -43,11 +43,13 @@ window.onload=function(){
         $url_conexion=$sql->row["url_conexion"];
         
         
-        $cmd="python3.7 ".RUTA_PROYECTO."motor/dofoto.py ".$camara_id." '".$url_conexion."'";
+        $cmd="python3.7 ".RUTA_PROYECTO."motor/dofoto.py ".$camara_id." '".$url_conexion."' '".RUTA_PROYECTO."'";
+        ?>
+                alert("<?= $cmd; ?>");
+        <?php
         echo "->".$cmd."<-";
         exec($cmd);
-        
-        sleep(2);
+        sleep(5);
         exec("chmod -R 777 ".RUTA_PROYECTO."admin/fotos_camara/".$camara_id.".png");
         
         ?>
